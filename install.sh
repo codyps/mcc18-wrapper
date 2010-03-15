0259b5da4f34783cf75e8f714a27219baade1c60
John Mesmon <jmesmon@gmail.com>
2010-03-15 19:31:58 -0400

    Don't make symlinks. they're annoying.

--- install.sh
+++ install.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-rmln () {
-	stat $1 >/dev/null 2>&1
-	if [ $? -ne 0 ]; then
-		echo "file \"$1\" not installed"
-		return
-	fi
-
-	readlink -q $1 >/dev/null
-	if [ $? -eq 0 ]; then
-		rm $1
-		echo "removed \"$1\""
-	else
-		echo "file \"$1\" is not the expected symlink"
-	fi
-}
 
 usage () {
 	echo "usage: $0 install <from dir> <to dir>" >/dev/stderr
@@ -30,12 +15,12 @@ fi
 if [ \( $1 = "install" \) -a \( $# -eq 3 \) ]; then
 	rdir=`cd $2; pwd -P`
 	for f in $files; do
-		ln -s $rdir/$f $3/$f
+		install $rdir/$f $3/$f
 		echo "installed \"$2/$f\" to \"$3/$f\""
 	done
 elif [ \( $1 = "uninstall" \) -a \( $# -eq 2 \) ]; then
 	for f in $files; do
-		rmln $2/$f
+		rm $2/$f
 	done
 else
 	usage
